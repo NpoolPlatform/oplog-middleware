209d9ebf1595e8b8a97d0bf22e870e6f857e53a2
Zhao KK <kikakkz@hotmail.com>
2023-08-13 06:35:16 +0000

    update ut

--- pkg/client/oplog/client_test.go
+++ pkg/client/oplog/client_test.go
@@ -112,6 +112,9 @@ func TestOpLog(t *testing.T) {
 	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	})
+	monkey.Patch(grpc2.GetGRPCConnV1, func(service string, recvMsgBytes int, tags ...string) (*grpc.ClientConn, error) {
+		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	})
 
 	t.Run("create", create)
 	t.Run("update", update)
